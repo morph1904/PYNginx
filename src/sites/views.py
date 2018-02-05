9b15ba0caa8834dbe2a2e3c11f7b78e21c3e28d9
ByteSense <morph1904@gmail.com>
2018-02-05 20:34:18 +0000

    Included basic setup steps

--- src/sites/views.py
+++ src/sites/views.py
@@ -3,7 +3,8 @@ from django.views.generic import ListView
 from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 from braces.views import FormMessagesMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .nginx import nginx as nginx_helper
+import nginx
+import subprocess as shell
 
 from . import models
 from .forms import SiteForm
@@ -47,4 +48,27 @@ class SiteDelete(FormMessagesMixin, LoginRequiredMixin, DeleteView):
     success_url = '/sites/'
 
 
+def nginx_create(site):
+    conf = nginx.Conf()
+    server = nginx.Server()
 
+    server.add(
+        nginx.Key('listen', '80'),
+        nginx.Key('server_name', site.domain),
+        nginx.Location('/',
+                       nginx.Key('proxy_pass', site.proxy_destination),
+                       nginx.Key('proxy_http_version', '1.1'),
+                       nginx.Key('proxy_set_header', 'Connection $http_connection'),
+                       nginx.Key('proxy_set_header', 'Origin http://$host'),
+                       nginx.Key('proxy_set_header', 'Upgrade $http_upgrade'),
+                       )
+    )
+    conf.add(server)
+    nginx.dumpf(conf, '/etc/nginx/sites-enabled' + site.domain + '.conf')
+
+    return True
+
+
+def ssl_create(site):
+    nginxstop = shell.call('service nginx stop', shell=True)
+    nginxstart = shell.call('service nginx start', shell=True)
